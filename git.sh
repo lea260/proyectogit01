4fd23797731d03d622e57df19736d9b5a09a5449
Thomas Villagran <liantorrio@gmail.com>
2022-05-02 15:51:16 -0300

    sol con

--- git.sh
+++ git.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-echo "Hello World!" 
+echo "Hola mundo" 
 # echo is similar to print statement.
+<<<<<<< HEAD
 git config --global user.name 'Lucas Clavijo' 
 git config --global user.email 'lucas15redes@gmail.com'
 git config --global color.ui true
 git config --global pull.rebase true
 git config --global credential.username "Novakronoo"
 git branch --set-upstream-to=origin/master master
+=======
+git config --global user.name 'Thomas Villagran' 
+git config --global user.email 'liantorrio@gmail.com'
+git config --global color.ui true
+git config --global pull.rebase true
+git config --global credential.username "TexasXThomas"
+# git branch --set-upstream-to=origin/master master
+>>>>>>> 4d3af39 (archivonuevo)
 git config --global alias.lodg 'log --oneline --decorate --graph'
 git config --global alias.conflict 'diff --name-only --diff-filter=U'
 # git config --global --get-regexp alias
