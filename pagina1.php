5659338a821abf165bd213b2cc86be59332ceb78
Diego <diegoacunaowo@gmail.com>
2022-05-21 13:44:19 -0300

    agrege contenido en pagina1.php

---
+++ pagina1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Pagina 1</h1>
+  <?php var_dump($_GET);
+$parametro01 = $_GET['parametro01'];
+$parametro02 = $_GET['parametro02'];?>
+  <p>Este es el parametro 01: <?=$parametro01;?></p>
+  <p>Este es el parametro 02: <?=$parametro02;?></p>
+
+
+  <p </body>
+
+</html>
