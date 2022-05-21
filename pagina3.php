9104c10c4e0e9dcd003f23471e364262a090aa0e
Diego <diegoacunaowo@gmail.com>
2022-05-21 14:34:10 -0300

    ahjhjhj

--- pagina3.php
+++ pagina3.php
@@ -5,18 +5,20 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Pagina1</title>
+  <title>Pagina3</title>
 </head>
 
 <body>
-  <h1>Pagina 1</h1>
-  <?php var_dump($_GET);
-$parametro01 = $_GET['parametro01'];
-$parametro02 = $_GET['parametro02'];?>
+  <h1>pagina 03</h1>
+  <?php var_dump($_POST);
+$parametro01 = $_POST['nombre'];
+$parametro02 = $_POST['edad'];?>
   <p>Este es el parametro 01: <?=$parametro01;?></p>
   <p>Este es el parametro 02: <?=$parametro02;?></p>
-
-
-  <p </body>
+  <?php if (!isset($_POST['apellido'])) {
+    echo "No defini en apellido en el formulario ";
+}
+;?>
+</body>
 
 </html>
