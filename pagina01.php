56a2d02a4acc12af4e1ac7b26aae61d6a9cecaa6
Sr.X <daviddossf@gmail.com>
2022-05-21 14:12:56 -0300

    Creo que entendí pero no tengo idea

--- pagina01.php
+++ pagina01.php
@@ -5,12 +5,16 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Página1</title>
 </head>
 
 <body>
-  <h1>pagina 01</h1>
-  <?php var_dump($_GET);?>
+  <h1>Página 1</h1>
+  <?php var_dump($_GET);
+$param01 = $_GET['param01'];
+$param02 = $_GET['param02'];?>
+  <p>El parámetro 1 es: <?=$param01;?></p>
+  <p>El parámetro 2 es: <?=$param02;?></p>
 
 </body>
 
---
+++ pagina02.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Personita</title>
+</head>
+
+<body>
+  <h1>Crear Persona</h1>
+  <form action="persona.php" method="post">
+    <label for="">Nombre</label>
+    <input type="text" name="nombre">
+    <label for="">Edad</label>
+    <input type="text" name="edad">
+    <input type="submit" value="enviar">
+  </form>
+</body>
+
+</html>
