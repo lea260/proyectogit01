97de6c98cc7659183a3d25484b1ed954e92322cc
TexasXThomas <liantorrio@gmail.com>
2022-05-21 14:19:56 -0300

    formulario nombre y edad

---
+++ pagina3.php
@@ -0,0 +1,20 @@
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h3>pagina 3</h3>
+  <?php var_dump($_GET);
+$años  = $_POST['edad'];
+$person = $_POST['nombre'];?>
+  <h1> bhola <?=$person;?> y tiene <?=$años;?> </h1>
+
+
+</body>
+
+</html>
