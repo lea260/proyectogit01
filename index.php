774731b127b123831ae96616ce0ae22787497eba
Sr.X <daviddossf@gmail.com>
2022-05-21 13:56:04 -0300

    Implementación de pagina01.php e index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>pagina de inicio</h1>
+  <ul>
+    <li><a href="pagina01.php?param01=hola&param02=mundo">pagina 1</a></li>
+    <li><a href="pagina02.php">pagina 2</a></li>
+  </ul>
+</body>
+
+</html>
