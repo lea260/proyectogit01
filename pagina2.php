be3ea975877add719acbf6908f26975d23c40d0e
TexasXThomas <liantorrio@gmail.com>
2022-05-26 22:30:56 -0300

    Fue la mas modificada, teniendo un radio button, select y checkbox los cuales son multiples, sino se entendio se explicara en clase

--- pagina2.php
+++ pagina2.php
@@ -17,11 +17,51 @@
       <label for="">edad</label>
       <input type="text" name="edad">
       <label for="">apellido</label>
-      <input type="text" name="apellido">
-      <label for="">telefonos</label>
-      <input type="text" name="tel[];">
-      <input type="submit" value="enviar">
+      <input type="text" name="apellido"> <br>
 
+
+      <form name="pais" action="pagina3.php" method="post"">
+        <label for=" paiss">seleccione un pais</label> <br>
+        <select name="paises[]" multiple="multiples">
+          <option value="uruguay"> Uruguay </option>
+          <option value="chile"> Chile </option>
+          <option value="paraguay"> Paraguay </option>
+          <option value="argentina"> Argentina </option>
+          <option value="ee.uu"> EE.UU </option>
+        </select>
+
+        <label for="tels1">
+          <input type="checkbox" id="telefono1" name="tels[]" value="telefonoUY">09213456</label><br>
+        <label for="tels2">
+          <input type="checkbox" id="telefono2" name="tels[]" value="telefonoEU"> 2418 7777 </label><br>
+        <label for="tels3">
+          <input type="checkbox" id="telefono3" name="tels[]" value="telefonoARG"> +549223123-4567</label><br>
+        <label for="tels4">
+          <input type="checkbox" id="telefono4" name="tels[]" value="telefonoCHL"> +561829124</label><br>
+        <input type="submit" name="submit" value="Enviarselo" />
+      </form>
+
+      <div class="categorias">
+        <h2>¿Usted es?</h2>
+        <input type="radio" name="categoria" class="categorias" value="estudiante"> Estudiante
+        <input type="radio" name="categoria" class="categorias" value="profesor"> Profesor
+        <input type="radio" name="categoria" class="categorias" value="madre/padre"> Madre/Padre
+      </div>
+
+
+
+
+
+      <hr> <br>
+
+    </form>
+
+
+    </form>
+
+
+
+    </form>
     </form>
   </body>
 
