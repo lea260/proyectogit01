95eba3a90c795741b50c4e283fa2fdd672712df6
TexasXThomas <liantorrio@gmail.com>
2022-05-26 22:32:50 -0300

    Se agregaron los codigos respectivos del checkbox, select y radio button este ultimo no logre que funcionara como yo esperaba.

--- pagina3.php
+++ pagina3.php
@@ -10,20 +10,46 @@
 <body>
   <h3>pagina 3</h3>
   <?php var_dump($_POST);
-$anios  = $_POST['edad'];
-$person = $_POST['nombre'];
-$apes   = $_POST['apellido'];
-$teles  = $_POST['tel[];'];
+$anios   = $_POST['edad'];
+$person  = $_POST['nombre'];
+$apes    = $_POST['apellido'];
+$telsis  = $_POST['tels'];
+$paisol  = $_POST['paises'];
+$radiosl = $_POST['categorias'];
 if (!isset($_POST['apellido'])) {
     echo "no definiste el apellido en el forms";
 }
-;?>
+$paisol = $_POST['paises'];
+foreach ($paisol as $valor) {
+    echo $valor . "<br>";
+}
+$telsis = $_POST['tels'];
+foreach ($telsis as $telefs) {
+    echo $telefs . "<br>";
+}
+if (!isset($_POST['categorias'])) {
+    $radiosl = $_POST['categorias'];
+} else {
+    $radiosl = "";
+}
+if ($radiosl == "") {
+    array_push($campos, "selecciona una opcion.");
+}
+
+?>
   <h1> bhola <?=$person;?> y tiene
     <?=$anios;?> y
     tiene el apellido
-    <?=$apellido;?> ademas
-    de su telefono que es <?=$tel[];?>
+    <?=$apes;?>
+
+
+
+
+
   </h1>
+
+
+
 
 
 </body>
